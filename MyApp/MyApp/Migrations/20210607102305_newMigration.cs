394f7bd8f81c5d4b55001c01d7031815f004dcb9
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2021-06-07 14:26:00 +0400

    migrations

---
+++ MyApp/MyApp/Migrations/20210607102305_newMigration.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyApp.Migrations
+{
+    public partial class newMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "statementName",
+                table: "Statements",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "phoneNumber",
+                table: "Statements",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statements_statementName",
+                table: "Statements",
+                column: "statementName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Statements_statementName",
+                table: "Statements");
+
+            migrationBuilder.DropColumn(
+                name: "phoneNumber",
+                table: "Statements");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "statementName",
+                table: "Statements",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
